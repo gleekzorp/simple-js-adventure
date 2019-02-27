d47d528a659ba44a75fbb164fd3283c8166fea58
gleekzorp <gleek.dan@gmail.com>
2019-02-27 12:01:00 -0700

    Forest scaredWrapper didn't have a try a new adventure button.  Organized the scripts and styles into their own files.

---
+++ scripts/forestScripts.js
@@ -0,0 +1,19 @@
+function notScared() {
+    document.getElementById('welcomeWrapper').style.display='none'
+    document.getElementById('helpGirlWrapper').style.display='block'
+}
+
+function scared() {
+    document.getElementById('welcomeWrapper').style.display='none'
+    document.getElementById('scaredWrapper').style.display='block'
+}
+
+function axe() {
+    document.getElementById('helpGirlWrapper').style.display='none'
+    document.getElementById('axeWrapper').style.display='block'
+}
+
+function spoon() {
+    document.getElementById('helpGirlWrapper').style.display='none'
+    document.getElementById('spoonWrapper').style.display='block'
+}
